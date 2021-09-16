72bd123c2c7d1d3aa1a9a1080565483298fd352e
GMWGMI <andrew@teller.finance>
2021-09-16 11:35:01 -0400

    fix api

--- market-api-server/lib/api-interface.js
+++ market-api-server/lib/api-interface.js
@@ -20,7 +20,7 @@ import https from 'https'
 import APIHelper from './api-helper.js'
 
 import AccessHelper from './access-helper.js'
-//import ApplicationManager from './application-manager.js'
+ import ApplicationManager from './application-manager.js'
 
 export default class APIInterface  {
 
