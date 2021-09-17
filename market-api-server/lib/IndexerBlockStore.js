1a40e1dcb010ecebfc7f8d767134a28e96f29e8e
andy <andy@jonezie.com>
2021-09-16 23:43:07 -0400

    indexer block store

--- market-api-server/lib/IndexerBlockStore.js
+++ market-api-server/lib/IndexerBlockStore.js
@@ -4,7 +4,7 @@
  
 import web3 from 'web3'
 
-export default class IndexerTellerOptions{
+export default class IndexerBlockStore{
 
     static async modifyLedgerByEvent(event,mongoInterface){ 
   
@@ -22,74 +22,63 @@ export default class IndexerTellerOptions{
  
         eventName = eventName.toLowerCase() 
 
+
+        if(eventName == 'nftsale'){
+            
+            let sellerAddress = web3.utils.toChecksumAddress( outputs['0'] )
+            
+            let buyerAddress = web3.utils.toChecksumAddress( outputs['1'] )
+            
+            let nftContractAddress = web3.utils.toChecksumAddress( outputs['2'] )
+            
+            let nftTokenId = parseInt( outputs['3'] )
+            let currencyTokenAddress = web3.utils.toChecksumAddress( outputs['4'] )
+            
+            let currencyTokenAmount = web3.utils.toChecksumAddress( outputs['5'] )
+             
+            
+            await IndexerTellerOptions.nftSale( sellerAddress, buyerAddress, nftContractAddress, nftTokenId, currencyTokenAddress, currencyTokenAmount, mongoInterface )
+             
+        }
         
-        if(eventName == 'optioncreated'){
-            let optionId =  parseInt(outputs['0'])
-            let creator = web3.utils.toChecksumAddress( outputs['1'] )
+        if(eventName == 'nonceburned'){
+            
+            let creator = web3.utils.toChecksumAddress( outputs['0'] )
+            let nonce = ( outputs['1'] )
             
             
-            await IndexerTellerOptions.optionCreated( optionId, creator,  mongoInterface )
+            await IndexerTellerOptions.nonceBurned( creator, nonce, mongoInterface )
              
         }
          
-        if(eventName == 'optionfilled'){
- 
-            let optionId =   parseInt(outputs['0'])
-  
-            await IndexerTellerOptions.setOptionStatus(optionId, 'filled', mongoInterface )
-             
-        }
-           
-        if(eventName == 'optionexercised'){
- 
-            let optionId =   parseInt(outputs['0'])
-  
-            await IndexerTellerOptions.setOptionStatus(optionId, 'exercised', mongoInterface )
-             
-        }
-         
-        if(eventName == 'optioncancelled'){
- 
-            let optionId =   parseInt(outputs['0'])
-  
-            await IndexerTellerOptions.setOptionStatus(optionId, 'cancelled', mongoInterface )
-             
-        }
-        if(eventName == 'optionexpired'){
- 
-            let optionId =   parseInt(outputs['0'])
-  
-            await IndexerTellerOptions.setOptionStatus(optionId, 'expired', mongoInterface )
-             
-        }
-         
+       
 
     }
  
 
-   static async setOptionStatus(optionId, newStatus, mongoInterface){
+   static async nonceBurned(orderCreator, nonce, mongoInterface){
 
-       let collectionName = 'teller_options' 
+       let collectionName = 'burned_nonces' 
 
-       let existing = await mongoInterface.findOne(collectionName, {optionId: optionId }  )
+       let existing = await mongoInterface.findOne(collectionName, {orderCreator: orderCreator, nonce:nonce }  )
 
        if(existing){            
-           await mongoInterface.updateOne(collectionName, {optionId: optionId}, {status: newStatus }   )
+           await mongoInterface.insertOne(collectionName, {orderCreator: orderCreator, nonce:nonce }   )
        }
    }
 
-   
-   static async optionCreated( optionId, creator, mongoInterface ){
 
-    let collectionName = 'teller_options'    
+   static async nftSale( sellerAddress, buyerAddress, nftContractAddress, nftTokenId, currencyTokenAddress, currencyTokenAmount, mongoInterface){
+
+    let collectionName = 'nft_sale' 
+
+            
+    await mongoInterface.insertOne(collectionName, {  sellerAddress:sellerAddress, buyerAddress:buyerAddress, nftContractAddress:nftContractAddress, nftTokenId:nftTokenId, currencyTokenAddress:currencyTokenAddress, currencyTokenAmount:currencyTokenAmount }   )
     
-    let existing = await mongoInterface.findOne(collectionName, {optionId: optionId }  )
-
-    if(!existing){ 
-      
-        await mongoInterface.insertOne(collectionName, { creator:creator, optionId: optionId , status:'created'}   )
-    }
 }
+
+   
+   
 
 
 
